8f397a919908bdeb4cc213f5f5dec0f7551f6837
Camilo Figueroa <cigotete@gmail.com>
2024-05-06 15:14:27 -0500

    Exploring another arrange of agents. This time also it is defined team Leaders connection order.

--- main.py
+++ main.py
@@ -17,17 +17,35 @@ llm_config_gpt3 = {
   "config_list": config_list_gpt3
 }
 
-# List of agents
-agents = [ConversableAgent(name=f"Agent{i}", llm_config=False) for i in range(5)]
+# Sequential Team Operations
+# Create an empty directed graph
 
-# Dictionary defining how are relations between agents.
-allowed_speaker_transitions_dict = {
-    agents[0]: [agents[1], agents[2], agents[3], agents[4]],
-    agents[1]: [agents[0]],
-    agents[2]: [agents[0]],
-    agents[3]: [agents[0]],
-    agents[4]: [agents[0]],
-}
+speaker_transitions_dict = {}
+teams = ["A", "B", "C"]
+team_size = 5
 
-# Visualizing relation.
-visualize_speaker_transitions_dict(allowed_speaker_transitions_dict, agents)
+
+def get_agent_of_name(agents, name) -> ConversableAgent:
+    for agent in agents:
+        if agent.name == name:
+            return agent
+
+
+# Create a list of 15 agents 3 teams x 5 agents
+agents = [ConversableAgent(name=f"{team}{i}", llm_config=False) for team in teams for i in range(team_size)]
+
+# Loop through each team and add members and their connections
+for team in teams:
+    for i in range(team_size):
+        member = f"{team}{i}"
+        # Connect each member to other members of the same team
+        speaker_transitions_dict[get_agent_of_name(agents, member)] = [
+            get_agent_of_name(agents, name=f"{team}{j}") for j in range(team_size) if j != i
+        ]
+
+# Team leaders connection
+print(get_agent_of_name(agents, name="B0"))
+speaker_transitions_dict[get_agent_of_name(agents, "A0")].append(get_agent_of_name(agents, name="B0"))
+speaker_transitions_dict[get_agent_of_name(agents, "B0")].append(get_agent_of_name(agents, name="C0"))
+
+visualize_speaker_transitions_dict(speaker_transitions_dict, agents)
